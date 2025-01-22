d339cee093995fd157d18fe78c0a3bb9ea77b691
unknown <rithvikaalapati1@gmaail.com>
2025-01-22 14:36:55 +0530

    Fix CSS import path in IntroPage.jsx

--- src/pages/IntroPage.jsx
+++ src/pages/IntroPage.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './intropage.css';
+import './Intropage.css';
 
 const IntroPage = () => {
   const [showSubtitle, setShowSubtitle] = useState(false);
